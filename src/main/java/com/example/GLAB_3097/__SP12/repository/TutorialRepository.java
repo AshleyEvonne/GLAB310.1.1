84a47ae9077f3215e544fdcb338e952807f7b8e3
AshleyEvonne <ashleyevonne345@gmail.com>
2024-09-18 14:14:57 -0400

    Spring Boot SOAP Web service

---
+++ src/main/java/com/example/GLAB_3097/__SP12/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.example.GLAB_3097.__SP12.repository;
+
+import com.example.GLAB_3097.__SP12.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@RepositoryRestResource(path="learningtutorials")
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>
+        // Tutorial → Entity type and Long --> primary key
+{
+    //   findByPublished is a custom method()
+    List<Tutorial> findByPublished(boolean published);
+}
+
